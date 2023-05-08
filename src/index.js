402185c6896811b829dc6fc2c9e2f5ae1de92938
Daniel Prates <danprates94@gmail.com>
2023-05-08 20:43:46 -0300

    feat: add Three Sums

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const twoSums = require('./two-sums')
 const maxConsecutivesOnes = require('./max-consecutives-ones')
 const longestSubstringWithoutRepeatingCharacters = require('./longest-substring-without-repeating-characters')
+const threeSums = require('./three-sums')
 
 twoSums()
 maxConsecutivesOnes()
 longestSubstringWithoutRepeatingCharacters()
+threeSums()
